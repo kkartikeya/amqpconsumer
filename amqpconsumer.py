7f552214f6264f494112384b8590118a26afcfc4
KK <kk@retailnext.net>
2018-05-03 17:39:18 -0700

    Initial version of AMQP Consumer

---
+++ amqpconsumer.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+
+import configparser
+import pika
+from com.kkartikeya.home.weather.weather_pb2 import Weather
+
+CONFIG_FILE_PATH='/Users/kk/work/personal/github.com/configuration/config.properties'
+
+def getAMQPURL():
+	config=configparser.RawConfigParser()
+	config.read(CONFIG_FILE_PATH)
+
+	return config.get('Messaging', 'CLOUDAMQP_URL')
+
+def consumeAMQPMessages( queue ):
+    AMQPURL=getAMQPURL()
+    params = pika.URLParameters(AMQPURL)
+    connection = pika.BlockingConnection(params)
+    channel = connection.channel() # start a channel
+
+    channel.queue_declare(queue)
+
+    def callback(ch, method, properties, body):
+        currentWeather=Weather()
+        currentWeather.ParseFromString(body)
+        print(" [x] Received %s" % currentWeather)
+
+    channel.basic_consume(callback,
+                          queue,
+                          no_ack=True)
+
+    print(' [*] Waiting for messages:')
+    channel.start_consuming()
+
+
+def main():
+    queue='weather'
+    consumeAMQPMessages(queue)
+
+if __name__ == "__main__":
+    main()
